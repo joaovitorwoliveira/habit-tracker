046d6145b5515e881bf73b9d9989d6737e01813b
Joao Vitor Oliveira <joaovitorwoliveira@gmail.com>
2024-06-21 15:32:14 -0300

    01 - Implementando Autenticacao com Clerk

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      <p>Essa é uma rota autenticada</p>
+      <Button variant="blue">Click me</Button>
+    </>
+  );
+}
